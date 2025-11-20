d0091761bccebfe6a53df16c4a82dbc534bbf040
Denis Carriere <carriere.denis@gmail.com>
2025-11-20 17:36:04 -0500

    add data

--- clickhouse-evm-transfers/src/logs.rs
+++ clickhouse-evm-transfers/src/logs.rs
@@ -18,23 +18,18 @@ pub fn set_template_log(encoding: &Encoding, log: &impl LogAddress, log_index: u
     row.set("log_index", log_index as u32);
     row.set("log_address", bytes_to_string(log.get_address(), encoding));
     row.set("log_ordinal", log.get_ordinal());
-
-    // handle if topic0 exists, else set to empty string
-    if let Some(topic0) = log.get_topic0() {
-        row.set("log_topic0", bytes_to_hex(topic0)); // keep hex encoding for topic0
-    } else {
-        row.set("log_topic0", "");
-    }
+    row.set("log_topics", {
+        let topics: Vec<String> = log.get_topics().iter().map(|topic| bytes_to_hex(topic)).collect();
+        topics.join(",")
+    });
+    row.set("log_data", bytes_to_hex(log.get_data()));
 }
 // Trait to abstract over different log types
 pub trait LogAddress {
     fn get_address(&self) -> &Vec<u8>;
     fn get_ordinal(&self) -> u64;
     fn get_topics(&self) -> &Vec<Vec<u8>>;
-    fn get_topic0(&self) -> Option<&Vec<u8>> {
-        self.get_topics().get(0)
-    }
-    fn _get_data(&self) -> &Vec<u8>;
+    fn get_data(&self) -> &Vec<u8>;
 }
 
 impl LogAddress for pb::Log {
@@ -47,7 +42,7 @@ impl LogAddress for pb::Log {
     fn get_topics(&self) -> &Vec<Vec<u8>> {
         &self.topics
     }
-    fn _get_data(&self) -> &Vec<u8> {
+    fn get_data(&self) -> &Vec<u8> {
         &self.data
     }
 }
