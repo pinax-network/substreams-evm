b6ced7014fc350f55c9330aa2ddd0442cfe74e2f
Denis Carriere <carriere.denis@gmail.com>
2025-11-20 12:38:52 -0500

    update protos

--- proto/src/pb/mod.rs
+++ proto/src/pb/mod.rs
@@ -1,68 +1,23 @@
 // @generated
-pub mod dex {
-    pub mod balancer {
-        // @@protoc_insertion_point(attribute:dex.balancer.v1)
-        pub mod v1 {
-            include!("dex.balancer.v1.rs");
-            // @@protoc_insertion_point(dex.balancer.v1)
-        }
+pub mod balancer {
+    // @@protoc_insertion_point(attribute:balancer.v1)
+    pub mod v1 {
+        include!("balancer.v1.rs");
+        // @@protoc_insertion_point(balancer.v1)
     }
-    pub mod bancor {
-        // @@protoc_insertion_point(attribute:dex.bancor.v1)
-        pub mod v1 {
-            include!("dex.bancor.v1.rs");
-            // @@protoc_insertion_point(dex.bancor.v1)
-        }
+}
+pub mod bancor {
+    // @@protoc_insertion_point(attribute:bancor.v1)
+    pub mod v1 {
+        include!("bancor.v1.rs");
+        // @@protoc_insertion_point(bancor.v1)
     }
-    pub mod curvefi {
-        // @@protoc_insertion_point(attribute:dex.curvefi.v1)
-        pub mod v1 {
-            include!("dex.curvefi.v1.rs");
-            // @@protoc_insertion_point(dex.curvefi.v1)
-        }
-    }
-    pub mod justswap {
-        // @@protoc_insertion_point(attribute:dex.justswap.v1)
-        pub mod v1 {
-            include!("dex.justswap.v1.rs");
-            // @@protoc_insertion_point(dex.justswap.v1)
-        }
-    }
-    pub mod sunpump {
-        // @@protoc_insertion_point(attribute:dex.sunpump.v1)
-        pub mod v1 {
-            include!("dex.sunpump.v1.rs");
-            // @@protoc_insertion_point(dex.sunpump.v1)
-        }
-    }
-    pub mod sunswap {
-        // @@protoc_insertion_point(attribute:dex.sunswap.v1)
-        pub mod v1 {
-            include!("dex.sunswap.v1.rs");
-            // @@protoc_insertion_point(dex.sunswap.v1)
-        }
-    }
-    pub mod uniswap {
-        // @@protoc_insertion_point(attribute:dex.uniswap.v1)
-        pub mod v1 {
-            include!("dex.uniswap.v1.rs");
-            // @@protoc_insertion_point(dex.uniswap.v1)
-        }
-        // @@protoc_insertion_point(attribute:dex.uniswap.v2)
-        pub mod v2 {
-            include!("dex.uniswap.v2.rs");
-            // @@protoc_insertion_point(dex.uniswap.v2)
-        }
-        // @@protoc_insertion_point(attribute:dex.uniswap.v3)
-        pub mod v3 {
-            include!("dex.uniswap.v3.rs");
-            // @@protoc_insertion_point(dex.uniswap.v3)
-        }
-        // @@protoc_insertion_point(attribute:dex.uniswap.v4)
-        pub mod v4 {
-            include!("dex.uniswap.v4.rs");
-            // @@protoc_insertion_point(dex.uniswap.v4)
-        }
+}
+pub mod curvefi {
+    // @@protoc_insertion_point(attribute:curvefi.v1)
+    pub mod v1 {
+        include!("curvefi.v1.rs");
+        // @@protoc_insertion_point(curvefi.v1)
     }
 }
 pub mod evm {
@@ -72,5 +27,48 @@ pub mod evm {
             include!("evm.transfers.v1.rs");
             // @@protoc_insertion_point(evm.transfers.v1)
         }
+    }
+}
+pub mod justswap {
+    // @@protoc_insertion_point(attribute:justswap.v1)
+    pub mod v1 {
+        include!("justswap.v1.rs");
+        // @@protoc_insertion_point(justswap.v1)
+    }
+}
+pub mod sunpump {
+    // @@protoc_insertion_point(attribute:sunpump.v1)
+    pub mod v1 {
+        include!("sunpump.v1.rs");
+        // @@protoc_insertion_point(sunpump.v1)
+    }
+}
+pub mod sunswap {
+    // @@protoc_insertion_point(attribute:sunswap.v1)
+    pub mod v1 {
+        include!("sunswap.v1.rs");
+        // @@protoc_insertion_point(sunswap.v1)
+    }
+}
+pub mod uniswap {
+    // @@protoc_insertion_point(attribute:uniswap.v1)
+    pub mod v1 {
+        include!("uniswap.v1.rs");
+        // @@protoc_insertion_point(uniswap.v1)
+    }
+    // @@protoc_insertion_point(attribute:uniswap.v2)
+    pub mod v2 {
+        include!("uniswap.v2.rs");
+        // @@protoc_insertion_point(uniswap.v2)
+    }
+    // @@protoc_insertion_point(attribute:uniswap.v3)
+    pub mod v3 {
+        include!("uniswap.v3.rs");
+        // @@protoc_insertion_point(uniswap.v3)
+    }
+    // @@protoc_insertion_point(attribute:uniswap.v4)
+    pub mod v4 {
+        include!("uniswap.v4.rs");
+        // @@protoc_insertion_point(uniswap.v4)
     }
 }
