eaab7f7900661a8926baeca0af3436dce2870e63
Denis Carriere <carriere.denis@gmail.com>
2025-11-20 12:54:40 -0500

    update yaml

--- dex/sunpump/src/store.rs
+++ dex/sunpump/src/store.rs
@@ -7,14 +7,24 @@ pub fn store_token_create(sunpump: pb::Events, store: StoreSetProto<pb::TokenCre
     for trx in sunpump.transactions.iter() {
         for log in trx.logs.iter() {
             // ---- TokenCreate ----
-            if let Some(pb::log::Log::TokenCreate(token_create)) = &log.log {
+            if let Some(pb::log::Log::TokenCreate(events)) = &log.log {
                 let payload = pb::TokenCreate {
-                    token_address: token_create.token_address.clone(),
+                    token_address: events.token_address.clone(),
                     factory: log.address.clone(),
-                    token_index: token_create.token_index.clone(),
-                    creator: token_create.creator.clone(),
+                    token_index: events.token_index.clone(),
+                    creator: events.creator.clone(),
                 };
-                store.set(1, Hex::encode(&token_create.token_address), &payload);
+                store.set(1, Hex::encode(&events.token_address), &payload);
+            }
+            // ---- TokenCreateLegacy ----
+            if let Some(pb::log::Log::TokenCreateLegacy(event)) = &log.log {
+                let payload = pb::TokenCreate {
+                    token_address: event.token_address.clone(),
+                    factory: log.address.clone(),
+                    token_index: 0.to_string(),
+                    creator: event.creator.clone(),
+                };
+                store.set(1, Hex::encode(&event.token_address), &payload);
             }
         }
     }
