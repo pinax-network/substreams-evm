ecb38ba4599625d11499dae29238f843cde21a0a
Denis Carriere <carriere.denis@gmail.com>
2025-11-22 15:32:44 -0500

    remove examples

--- clickhouse-evm-transfers/migrations/v0.2.1.sql
+++
@@ -1,5 +0,0 @@
--- PROJECTION REMOVALS --
-ALTER TABLE native_transfer ON CLUSTER 'tokenapis-a' DROP PROJECTION prj_to_from_by_minute;
-ALTER TABLE trc20_transfer ON CLUSTER 'tokenapis-a' DROP PROJECTION prj_log_address_from_by_minute;
-ALTER TABLE trc20_transfer ON CLUSTER 'tokenapis-a' DROP PROJECTION prj_log_address_to_by_minute;
-ALTER TABLE trc20_transfer ON CLUSTER 'tokenapis-a' DROP PROJECTION prj_log_address_from_to_by_minute;
