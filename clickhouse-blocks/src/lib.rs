fc558f529dccd1573da834c5f74294ebf210ec53
Denis Carriere <carriere.denis@gmail.com>
2025-11-28 17:45:31 -0500

    add Clickhouse Blocks

---
+++ clickhouse-blocks/src/lib.rs
@@ -0,0 +1,20 @@
+use substreams::pb::substreams::Clock;
+
+use substreams::errors::Error;
+use substreams_database_change::pb::database::DatabaseChanges;
+
+#[substreams::handlers::map]
+pub fn db_out(clock: Clock) -> Result<DatabaseChanges, Error> {
+    let mut tables = substreams_database_change::tables::Tables::new();
+    set_clock(&clock, tables.create_row("blocks", [("block_num", clock.number.to_string())]));
+    Ok(tables.to_database_changes())
+}
+
+pub fn set_clock(clock: &Clock, row: &mut substreams_database_change::tables::Row) {
+    row.set("block_num", clock.number);
+    row.set("block_hash", format!("0x{}", clock.id));
+    if let Some(timestamp) = &clock.timestamp {
+        row.set("timestamp", timestamp.seconds);
+        row.set("minute", timestamp.seconds / 60);
+    }
+}
