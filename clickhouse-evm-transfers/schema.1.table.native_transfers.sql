ace03ac3a080893259af8deccc50eed945568c04
Denis Carriere <carriere.denis@gmail.com>
2025-11-29 15:06:41 -0500

    update native

--- clickhouse-evm-transfers/schema.1.table.native_transfers.sql
+++ clickhouse-evm-transfers/schema.1.table.native_transfers.sql
@@ -1,22 +1,28 @@
+-- EXTENDED transaction with more gas fields
+CREATE TABLE IF NOT EXISTS transactions AS TEMPLATE_TRANSACTION
+COMMENT 'Extended Transactions with additional gas/fee fields';
+ALTER TABLE transactions
+    -- extended transaction gas/fee --
+    ADD COLUMN IF NOT EXISTS tx_base_fee_per_gas        UInt64,
+    ADD COLUMN IF NOT EXISTS tx_transaction_fee         UInt256,
+    ADD COLUMN IF NOT EXISTS tx_burn_fee                UInt256,
+    ADD COLUMN IF NOT EXISTS tx_fee_paid                UInt256;
+
 -- Native TRX Transfer Transactions --
-CREATE TABLE IF NOT EXISTS native_transfers AS TEMPLATE_TRANSACTION
-COMMENT 'Native Transfer events';
-ALTER TABLE native_transfers
-    -- transfer --
-    ADD COLUMN IF NOT EXISTS `from`        String,
-    ADD COLUMN IF NOT EXISTS `to`          String,
-    ADD COLUMN IF NOT EXISTS amount        UInt256,
-
-    -- INDEXES --
-    ADD INDEX IF NOT EXISTS idx_amount (amount) TYPE minmax GRANULARITY 1,
-
-    -- PROJECTIONS --
-    -- count() --
-    ADD PROJECTION IF NOT EXISTS prj_from_count ( SELECT `from`, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY `from` ),
-    ADD PROJECTION IF NOT EXISTS prj_to_count ( SELECT `to`, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY `to` ),
-    ADD PROJECTION IF NOT EXISTS prj_from_to_count ( SELECT `from`, `to`, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY `from`, `to` ),
-    ADD PROJECTION IF NOT EXISTS prj_to_from_count ( SELECT `to`, `from`, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY `to`, `from` ),
-
-    -- minute: from | to --
-    ADD PROJECTION IF NOT EXISTS prj_from_by_minute ( SELECT `from`, minute, count() GROUP BY `from`, minute ),
-    ADD PROJECTION IF NOT EXISTS prj_to_by_minute ( SELECT `to`, minute, count() GROUP BY `to`, minute );
+CREATE TABLE IF NOT EXISTS calls AS TEMPLATE_TRANSACTION
+ENGINE = MergeTree
+ORDER BY (
+    minute, timestamp, block_num,
+    tx_index, call_index,
+    block_hash
+)
+COMMENT 'Calls with native value transfers';
+ALTER TABLE calls
+    -- call --
+    ADD COLUMN IF NOT EXISTS call_index         UInt32,
+    ADD COLUMN IF NOT EXISTS caller             String,
+    ADD COLUMN IF NOT EXISTS address            String,
+    ADD COLUMN IF NOT EXISTS value              UInt256,
+    ADD COLUMN IF NOT EXISTS gas_consumed       UInt64,
+    ADD COLUMN IF NOT EXISTS gas_limit          UInt64,
+    ADD COLUMN IF NOT EXISTS depth              UInt32;
