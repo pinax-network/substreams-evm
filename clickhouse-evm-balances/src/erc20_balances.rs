427c59b55653a86f173ac0ee2ce3b1a971839095
Denis Carriere <carriere.denis@gmail.com>
2025-12-01 20:41:40 -0500

    update balances

--- clickhouse-evm-balances/src/erc20_balances.rs
+++ clickhouse-evm-balances/src/erc20_balances.rs
@@ -3,17 +3,20 @@ use proto::pb::evm::balances::v1 as pb;
 use substreams::pb::substreams::Clock;
 use substreams_database_change::tables::Tables;
 
-use crate::{clock_key, set_clock};
+use crate::set_clock;
 
 pub fn process_events(encoding: &Encoding, tables: &mut Tables, clock: &Clock, events: &pb::Events) {
     for balance in events.balances.iter() {
         if let Some(contract) = &balance.contract {
-            let row = tables.create_row("erc20_balances", clock_key(clock));
+            let contract = bytes_to_string(&contract, encoding);
+            let account = bytes_to_string(&balance.account, encoding);
+            let key = [("contract", contract.to_string()), ("account", account.to_string())];
+            let row = tables.create_row("erc20_balances", key);
 
             set_clock(clock, row);
 
-            row.set("contract", bytes_to_string(&contract, encoding));
-            row.set("account", bytes_to_string(&balance.account, encoding));
+            row.set("contract", &contract);
+            row.set("account", &account);
             row.set("balance", &balance.balance);
         }
     }
