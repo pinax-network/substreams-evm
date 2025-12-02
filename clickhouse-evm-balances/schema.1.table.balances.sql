5bb66be306dbc9bfe66df5ab21e2ddaaedc1410f
Denis Carriere <carriere.denis@gmail.com>
2025-12-01 21:48:04 -0500

    remove median

--- clickhouse-evm-balances/schema.1.table.balances.sql
+++ clickhouse-evm-balances/schema.1.table.balances.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS erc20_balances (
     INDEX idx_balance (balance) TYPE minmax GRANULARITY 1,
 
     -- count() --
-    PROJECTION prj_contract_count ( SELECT median(balance), min(balance), max(balance), count(), max(block_num), min(block_num), max(timestamp), min(timestamp), max(minute), min(minute) GROUP BY contract ),
+    PROJECTION prj_contract_count ( SELECT min(balance), max(balance), count(), max(block_num), min(block_num), max(timestamp), min(timestamp), max(minute), min(minute) GROUP BY contract ),
 
     -- projections --
     PROJECTION prj_address_contract ( SELECT * ORDER BY address, contract ),
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS native_balances (
     INDEX idx_balance (balance) TYPE minmax GRANULARITY 1,
 
     -- count() --
-    PROJECTION prj_count ( SELECT median(balance), min(balance), max(balance), count(), max(block_num), min(block_num), max(timestamp), min(timestamp), max(minute), min(minute) ),
+    PROJECTION prj_count ( SELECT min(balance), max(balance), count(), max(block_num), min(block_num), max(timestamp), min(timestamp), max(minute), min(minute) ),
 )
 ENGINE = ReplacingMergeTree(block_num)
 ORDER BY (address)
