d2ba7f13b4788a755967bc9a0eb951ac71273f86
Denis Carriere <carriere.denis@gmail.com>
2025-12-04 13:54:24 -0500

    Update MV's

--- clickhouse-evm-transfers/schema.1.table.weth.sql
+++ clickhouse-evm-transfers/schema.1.table.weth.sql
@@ -1,5 +1,6 @@
 -- WETH Deposit/Withdrawal Logs --
 CREATE TABLE IF NOT EXISTS weth_deposit AS TEMPLATE_LOG
+TTL timestamp + INTERVAL 1 DAY
 COMMENT 'WETH Deposit events';
 ALTER TABLE weth_deposit
     -- event --
@@ -7,32 +8,13 @@ ALTER TABLE weth_deposit
     ADD COLUMN IF NOT EXISTS wad        UInt256,
 
     -- INDEXES --
-    ADD INDEX IF NOT EXISTS idx_wad (wad) TYPE minmax GRANULARITY 1,
-
-    -- PROJECTIONS --
-    -- count() --
-    ADD PROJECTION IF NOT EXISTS prj_dst_count ( SELECT dst, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY dst ),
-    ADD PROJECTION IF NOT EXISTS prj_log_address_dst_count ( SELECT log_address, dst, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY log_address, dst ),
-
-    -- minute --
-    ADD PROJECTION IF NOT EXISTS prj_dst_by_minute ( SELECT dst, minute, count() GROUP BY dst, minute ),
-    ADD PROJECTION IF NOT EXISTS prj_log_address_dst_by_minute ( SELECT log_address, dst, minute, count() GROUP BY log_address, dst, minute);
+    ADD INDEX IF NOT EXISTS idx_wad (wad) TYPE minmax GRANULARITY 1;
 
 -- WETH Withdrawal Logs --
 CREATE TABLE IF NOT EXISTS weth_withdrawal AS TEMPLATE_LOG
+TTL timestamp + INTERVAL 1 DAY
 COMMENT 'WETH Withdrawal events';
 ALTER TABLE weth_withdrawal
     -- event --
     ADD COLUMN IF NOT EXISTS src        String,
-    ADD COLUMN IF NOT EXISTS wad        UInt256,
-
-    -- INDEXES --
-    ADD INDEX IF NOT EXISTS idx_wad (wad) TYPE minmax GRANULARITY 1,
-
-    -- PROJECTIONS --
-    -- count() --
-    ADD PROJECTION IF NOT EXISTS prj_src_count ( SELECT src, count(),min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY src ),
-    ADD PROJECTION IF NOT EXISTS prj_log_address_src_count ( SELECT log_address, src, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY log_address, src ),
-    -- minute --
-    ADD PROJECTION IF NOT EXISTS prj_src_by_minute ( SELECT src, minute, count() GROUP BY src, minute ),
-    ADD PROJECTION IF NOT EXISTS prj_log_address_src_by_minute ( SELECT log_address, src, minute, count() GROUP BY log_address, src, minute);
+    ADD COLUMN IF NOT EXISTS wad        UInt256;
