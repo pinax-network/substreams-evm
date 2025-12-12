fda75127403c0ad3c89ec9f745bb8aa808fbe9f5
Denis Carriere <carriere.denis@gmail.com>
2025-12-12 15:49:40 -0500

    update pools

--- clickhouse-evm-dex/schema.4.view.pools.sql
+++ clickhouse-evm-dex/schema.4.view.pools.sql
@@ -1,30 +1,45 @@
 CREATE VIEW IF NOT EXISTS pools AS
 SELECT
-    -- initialize --
-    i.factory       AS factory,
-    i.pool          AS pool,
-    i.protocol      AS protocol,
+    -- transactions (summing) --
+    s.factory       AS factory,
+    s.pool          AS pool,
+    s.protocol      AS protocol,
+    s.transactions  AS transactions,
+
+    -- initialize (required) --
     i.block_num     AS initialize_block_num,
     i.timestamp     AS initialize_timestamp,
     i.tx_hash       AS initialize_tx_hash,
 
-    -- fees --
+    -- fees (optional) --
     f.fee           AS fee,
     f.block_num     AS fee_block_num,
     f.timestamp     AS fee_timestamp,
     f.tx_hash       AS fee_tx_hash,
 
-    -- tokens (Array) --
+    -- tokens (required >=2 token) --
     (
         SELECT arraySort(groupArrayDistinct(token))
         FROM state_pools_tokens
-        WHERE factory = i.factory AND pool = i.pool AND protocol = i.protocol
+        WHERE factory = s.factory AND pool = s.pool AND protocol = s.protocol
     ) AS tokens
-FROM state_pools_initialize AS i
+
+-- transactions (summing) --
+FROM state_pool_activity_summary AS s
+
+-- initialize (required) --
+JOIN state_pools_initialize AS i
+    ON s.protocol = i.protocol
+   AND s.factory = i.factory
+   AND s.pool = i.pool
+
 -- fees (optional) --
 LEFT JOIN state_pools_fees AS f
-    ON i.protocol = f.protocol
-   AND i.factory = f.factory
-   AND i.pool = f.pool
+    ON s.protocol = f.protocol
+   AND s.factory = f.factory
+   AND s.pool = f.pool
+
+-- must have at least 2 tokens --
 WHERE length(tokens) >= 2
+
 SETTINGS allow_experimental_correlated_subqueries = 1, join_use_nulls = 1;
