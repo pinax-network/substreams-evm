541f9d13846c922bcb7cb29d5bf4e32f625a29de
Denis Carriere <carriere.denis@gmail.com>
2025-12-12 15:31:55 -0500

    update pool

--- clickhouse-evm-dex/schema.4.view.pools.sql
+++ clickhouse-evm-dex/schema.4.view.pools.sql
@@ -1,39 +1,30 @@
 CREATE VIEW IF NOT EXISTS pools AS
 SELECT
-    t.factory AS factory,
-    t.pool AS pool,
-    t.protocol AS protocol,
-    t.tokens AS tokens,  -- Array of tokens
-
     -- initialize --
-    i.block_num  AS initialize_block_num,
-    i.timestamp  AS initialize_timestamp,
-    i.tx_hash    AS initialize_tx_hash,
+    i.factory       AS factory,
+    i.pool          AS pool,
+    i.protocol      AS protocol,
+    i.block_num     AS initialize_block_num,
+    i.timestamp     AS initialize_timestamp,
+    i.tx_hash       AS initialize_tx_hash,
 
     -- fees --
-    f.fee        AS fee,
-    f.block_num  AS fee_block_num,
-    f.timestamp  AS fee_timestamp,
-    f.tx_hash    AS fee_tx_hash
-FROM
-(
-    SELECT
-        factory,
-        pool,
-        protocol,
-        arraySort(groupArrayDistinct(token)) AS tokens
-    FROM state_pools_tokens
-    GROUP BY
-        factory,
-        pool,
-        protocol
-) AS t
+    f.fee           AS fee,
+    f.block_num     AS fee_block_num,
+    f.timestamp     AS fee_timestamp,
+    f.tx_hash       AS fee_tx_hash,
+
+    -- tokens (Array) --
+    (
+        SELECT arraySort(groupArrayDistinct(token))
+        FROM state_pools_tokens
+        WHERE factory = i.factory AND pool = i.pool AND protocol = i.protocol
+    ) AS tokens
+FROM state_pools_initialize AS i
+-- fees (optional) --
 LEFT JOIN state_pools_fees AS f
-    ON t.pool = f.pool
-   AND t.factory = f.factory
-   AND t.protocol = f.protocol
-LEFT JOIN state_pools_initialize AS i
-    ON t.pool = i.pool
-   AND t.factory = i.factory
-   AND t.protocol = i.protocol
-SETTINGS join_use_nulls = 1;
+    ON i.protocol = f.protocol
+   AND i.factory = f.factory
+   AND i.pool = f.pool
+WHERE length(tokens) >= 2
+SETTINGS allow_experimental_correlated_subqueries = 1, join_use_nulls = 1;
