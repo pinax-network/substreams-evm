2c92840c8bc6ff849e7bd079558aa8be075cfcf8
Denis Carriere <carriere.denis@gmail.com>
2026-02-01 12:50:04 -0500

    add to clickhouse

--- clickhouse-evm-transfers/schema.1.table.native_transfers.sql
+++ clickhouse-evm-transfers/schema.1.table.native_transfers.sql
@@ -15,9 +15,44 @@ CREATE TABLE IF NOT EXISTS block_rewards (
     minute                      UInt32 COMMENT 'toRelativeMinuteNum(timestamp)',
 
     -- block reward --
+    index                       UInt32,
     miner                       String,
     value                       UInt256,
     reason                      LowCardinality(String)
+)
+ENGINE = MergeTree
+ORDER BY block_num;
+
+-- Validator Withdrawals (post-Shanghai) --
+CREATE TABLE IF NOT EXISTS withdrawals (
+    -- block --
+    block_num                   UInt32,
+    block_hash                  String,
+    timestamp                   DateTime('UTC'),
+    minute                      UInt32 COMMENT 'toRelativeMinuteNum(timestamp)',
+
+    -- withdrawal --
+    index                       UInt32,
+    address                     String,
+    value                       UInt256
+)
+ENGINE = MergeTree
+ORDER BY block_num;
+
+-- Selfdestructs --
+CREATE TABLE IF NOT EXISTS selfdestructs (
+    -- block --
+    block_num                   UInt32,
+    block_hash                  String,
+    timestamp                   DateTime('UTC'),
+    minute                      UInt32 COMMENT 'toRelativeMinuteNum(timestamp)',
+
+    -- selfdestruct --
+    index                       UInt32,
+    tx_hash                     String,
+    from_address                String,
+    to_address                  String,
+    value                       UInt256
 )
 ENGINE = MergeTree
 ORDER BY block_num;
