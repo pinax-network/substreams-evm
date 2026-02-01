3af1ae02ac0958d319b1f50e427b8bd350a58a58
Denis Carriere <carriere.denis@gmail.com>
2026-02-01 12:48:40 -0500

    Update native transfers

---
+++ native-transfers/src/pb/mod.rs
@@ -0,0 +1,10 @@
+// @generated
+pub mod erc20 {
+    pub mod transfers {
+        // @@protoc_insertion_point(attribute:erc20.transfers.v1)
+        pub mod v1 {
+            include!("erc20.transfers.v1.rs");
+            // @@protoc_insertion_point(erc20.transfers.v1)
+        }
+    }
+}
