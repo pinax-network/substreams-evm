11e6329490555b4642a05b45ab16a04b9f75dec0
Denis Carriere <carriere.denis@gmail.com>
2026-02-03 21:31:25 -0500

    add refresh FINAL tables

---
+++ db-evm-balances-clickhouse/schema.2.mv.refresh.balances.sql
@@ -0,0 +1,23 @@
+-- ERC-20 Balances FINAL --
+-- Refreshable materialized view that maintains deduplicated (FINAL) balances
+-- Ordered by (contract, balance, address) to support efficient top/bottom holder queries per contract
+CREATE MATERIALIZED VIEW IF NOT EXISTS erc20_balances_final
+REFRESH EVERY 10 MINUTE
+ENGINE = ReplacingMergeTree(block_num)
+ORDER BY (contract, balance, address)
+AS
+SELECT *
+FROM erc20_balances FINAL
+WHERE balance > 0;
+
+-- Native Balances FINAL --
+-- Refreshable materialized view that maintains deduplicated (FINAL) balances
+-- Ordered by (contract, balance, address) to support efficient top/bottom holder queries per contract
+CREATE MATERIALIZED VIEW IF NOT EXISTS native_balances_final
+REFRESH EVERY 10 MINUTE
+ENGINE = ReplacingMergeTree(block_num)
+ORDER BY (address, balance)
+AS
+SELECT *
+FROM native_balances FINAL
+WHERE balance > 0;
