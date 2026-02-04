54e1e6d753150f6c34f231a31d4c8436d8a62bc1
Denis Carriere <carriere.denis@gmail.com>
2026-02-04 10:46:32 -0500

    update SQL

--- db-evm-balances-clickhouse/schema.2.mv.refresh.balances.sql
+++ db-evm-balances-clickhouse/schema.2.mv.refresh.balances.sql
@@ -2,7 +2,7 @@
 -- Refreshable materialized view that maintains deduplicated (FINAL) balances
 -- Ordered by (contract, balance, address) to support efficient top/bottom holder queries per contract
 CREATE MATERIALIZED VIEW IF NOT EXISTS erc20_balances_final
-REFRESH EVERY 10 MINUTE
+REFRESH EVERY 1 HOUR
 ENGINE = ReplacingMergeTree(block_num)
 ORDER BY (contract, balance, address)
 AS
@@ -14,7 +14,7 @@ WHERE balance > 0;
 -- Refreshable materialized view that maintains deduplicated (FINAL) balances
 -- Ordered by (contract, balance, address) to support efficient top/bottom holder queries per contract
 CREATE MATERIALIZED VIEW IF NOT EXISTS native_balances_final
-REFRESH EVERY 10 MINUTE
+REFRESH EVERY 1 HOUR
 ENGINE = ReplacingMergeTree(block_num)
 ORDER BY (address, balance)
 AS
